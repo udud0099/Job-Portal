296b3bf55bad981a02fe5a98fa2d4b5ee66dc730
udud0099 <udud0099@gmail.com>
2024-11-15 13:10:00 +0545

    mern jobportal backend server made

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http//localhost:5173",
+  Credentials: true,
+};
+app.use(cors(corsOptions));
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});

aae9fe9ebb41e46e635bbacd7ec598b019277b3e
udud0099 <udud0099@gmail.com>
2024-11-19 13:41:54 +0545

    3h:14:00 all backend code completed and image part only lest. we did it in cludme letur part

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,122 @@
+import { Job } from "../models/job.model.js";
+
+// admin post job
+export const postJob = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      requirements,
+      salary,
+      location,
+      jobType,
+      experience,
+      position,
+      companyId,
+    } = req.body;
+    const userId = req.id;
+
+    if (
+      !title ||
+      !description ||
+      !requirements ||
+      !salary ||
+      !location ||
+      !jobType ||
+      !experience ||
+      !position ||
+      !companyId
+    ) {
+      return res.status(400).json({
+        message: "something is missing.",
+        success: false,
+      });
+    }
+    const job = await Job.create({
+      title,
+      description,
+      requirements: requirements.split(","),
+      salary: Number(salary),
+      location,
+      jobType,
+      experienceLevel: experience,
+      position,
+      company: companyId,
+      created_by: userId,
+    });
+    return res.status(201).json({
+      message: "New job cread successfully",
+      job,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//student
+export const getAllJobs = async (req, res) => {
+  try {
+    const keyword = req.query.keyword || "";
+    const query = {
+      $or: [
+        { title: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+      ],
+    };
+    const jobs = await Job.find(query)
+      .populate({
+        path: "company",
+      })
+      .sort({ createdAt: -1 });
+    if (!jobs) {
+      return res.status(404).json({
+        message: "Jobs not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// student
+export const getJobById = async (req, res) => {
+  try {
+    const jobId = req.params.jobId;
+    const job = await Job.findById(jobId);
+    if (!job) {
+      return res.status(404).json({
+        message: "Jobs not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({ job, success: true });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// how many job created by admin
+export const getAdminJobs = async (req, res) => {
+  try {
+    const adminId = req.id;
+    const jobs = await Job.find({ created_by: adminId });
+    if (!jobs) {
+      return res.status(404).json({
+        message: "jobs not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      jobs,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import userRoute from "./routes/user.routes.js";
-import companyRoute from "./routes/company.routes.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
@@ -23,6 +25,8 @@ const PORT = process.env.PORT || 3000;
 // all api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/application", applicationRoute);
 
 // ("http://localhost:8000/api/v1/user/register");
 // ("http://localhost:8000/api/v1/user/login");
